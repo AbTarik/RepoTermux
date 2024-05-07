54df0d14b47507daa3dbc9afbff447e3bc86e63c
Tarik Aboullait <aboullaittarik@gmail.com>
2024-05-07 21:27:59 +0200

    modules implemented corrections

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import Module1 from '.modules/module1'
-import Module2 from '.modules/module2'
+import Module1 from './modules/module1'
+import Module2 from './modules/module2'
 
 alert(`${Module1.getA()}`)
 alert(`${Module2.getA()}`)
